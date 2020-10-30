332ddbc925c3c6728355ed5dcbfdca341e699ad0
Fernando <fernandols.cardoso@gmail.com>
2020-10-30 00:33:12 -0300

    Signals

--- meu_site/blog/models.py
+++ meu_site/blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.dispatch import receiver
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.text import slugify
+from django.db.models.signals import post_save
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -14,7 +17,7 @@ class Post(models.Model):
         ('publicado','Publicado'),
     )
     titulo = models.CharField(max_length=250)
-    slug   = models.SlugField(max_length=250)#meusite.com/noticias/campeonato-brasileiro-2020/01-02-2020
+    slug   = models.SlugField(max_length=250)
     autor  = models.ForeignKey(User,
                                on_delete=models.CASCADE)
     conteudo  = models.TextField()
@@ -38,5 +41,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.titulo
+
+@receiver(post_save, sender=Post)
+def inser_slug(sender,instance,**kwargs):
+    print (dir(kwargs))
+    print (dir(instance))
+    if not instance.slug:
+        instance.slug = slugify(instance.titulo)
+        return instance.save()
+    
 
 # Create your models here.
